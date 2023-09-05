8ac2c5d0179b5aa22366c370a5c7822816b9d9f3
ardi <ardifjar443@gmail.com>
2023-09-05 09:41:08 +0700

    template

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var myIframe = document.getElementById('myIframe');
+
+// Fungsi untuk mengganti sumber iframe
+function gantiSumberIframe(src) {
+    // Ganti URL sumber iframe dengan URL yang diinginkan
+    myIframe.src = src; // Ganti URL ini sesuai dengan yang Anda inginkan
+}
+
+// Menambahkan event listener ke tombol
+gantiSourceButton.addEventListener('click', gantiSumberIframe);
