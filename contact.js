fff30b78bb2c564b92f9fe25f4b78493d345b3fa
RATUAYU NURFAJAR <106720973+rratuan@users.noreply.github.com>
2023-09-09 14:14:52 +0700

    seventh commit

--- contact.js
+++ contact.js
@@ -4,6 +4,19 @@ const form = document.forms['ratu-contact-form']
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
+    .then(response => response.json())
+        .then(data => {
+            // Periksa jika pengiriman berhasil
+            if (data.result === 'success') {
+                // Kosongkan semua kolom input pada formulir
+                form.reset();
+                alert('Thank you, your message has been sent!');
+            } else {
+                alert('Terjadi kesalahan saat mengirim data. Silakan coba lagi.');
+            }
+        })
+        .catch(error => {
+            console.error('Terjadi kesalahan:', error);
+            alert('Sorry, an error occurred while sending the message. Please try again.');
+        });
 })
