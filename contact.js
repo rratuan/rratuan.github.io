5a8dd944c38c6e566f3ea0ae346ee640b48c146a
RATUAYU NURFAJAR <106720973+rratuan@users.noreply.github.com>
2023-09-09 12:05:56 +0700

    forth commit

--- contact.js
+++ contact.js
@@ -1,5 +1,5 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxEXm87gZIeKCLZtkEAe7wLZHZYl_k8VsMySXzX2V1apwlm11W2daZpMhcWZch6Dvg/exec'
-const form = document.forms['ratu-contact-form']
+const form = document.form['ratu-contact-form']
 
 form.addEventListener('submit', e => {
     e.preventDefault()
